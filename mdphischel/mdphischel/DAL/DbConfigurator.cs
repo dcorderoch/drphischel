d893b23996bdc007bc82941267395c957ef6f313
Manuel Zumbado <Manuel Zumbado>
2016-05-02 15:31:14 +0000

    Added DbConfigurator class

---
+++ mdphischel/mdphischel/DAL/DbConfigurator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Configuration;
+
+
+namespace mdphischel.DAL
+{
+    public class DBConfigurator
+    {
+        public static string ConnectionString
+
+        {
+
+            get
+
+            {
+                var mySetting = WebConfigurationManager.ConnectionStrings;
+                if (mySetting == null || string.IsNullOrEmpty(mySetting["DrPhischel"].ConnectionString))
+                    throw new Exception("Fatal error: missing connecting string in web.config file");
+                var conString = mySetting["DrPhischel"].ConnectionString;
+                return conString;
+            }
+
+        }
+
+    }
+}
