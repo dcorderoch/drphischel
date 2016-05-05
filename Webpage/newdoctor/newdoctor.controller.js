2724a2e8e63c4bf45379678fdc197b3ec23a4383
majesco <nicolas.j2007@gmail.com>
2016-05-05 15:26:22 -0600

    newDoctor controller added

---
+++ Webpage/newdoctor/newdoctor.controller.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+ 
+    angular
+        .module('app')
+        .controller('NewDoctorController', NewDoctorController);
+ 
+    NewDoctorController.$inject = ['$location', 'DoctorService', 'SpecialityService','FlashService', '$rootScope','$scope'];
+    function NewDoctorController(location, SpecialityService, DoctorService, FlashService, $rootScope,$scope) {
+        var vm = this;
+ 
+        //Tomar especialidades
+        $scope.medicSpecialities = []; //Read
+        //Crear
+        $scope.createDoctor= createDoctor;    //Create
+        
+
+        function initController() {
+            loadAllSpecialities();
+        }();
+        
+        function loadAllSpecialities() {
+            SpecialityService.GetAll()
+                .then(function (specialities) {
+                    $scope.medicSpecialities = specialities.data;
+            },function(){
+                 FlashService.Error("Error al cargar especialidades");       
+            });
+        }
+        
+        function createDoctor(){
+            
+            DoctorService.Create($scope.newDoc)
+                .then(function() {
+                    $scope.newDoc ={};   
+                },function(response){
+                    FlashService.Error('Error en la creacion de doctor');      
+                });
+        }
+    }
+ 
+})();
