00d0756f5bb4358ca70165354f5c16e4c2e95e75
majesco <nicolas.j2007@gmail.com>
2016-05-05 18:17:54 -0600

    Added code for acceptdoctor controller Added code for addspecialities controller

---
+++ Webpage/addSpecialities/addSpecialities.controller.js
@@ -0,0 +1,36 @@
+(function () {
+    'use strict';
+
+    angular   ///modulo al que pertenece
+        .module('app')
+        .controller('AddSpecialityController', AddSpecialityController);
+
+    AddSpecialityController.$inject = ['SpecialityService', '$rootScope', 'FlashService', '$scope'];
+    function AddSpecialityController(SpecialityService,$rootScope, FlashService, $scope) {
+        
+        $scope.order={};
+        var vm = this;
+        
+        $scope.addSpeciality= addSpeciality;  //variables locales y para vistas
+
+        function addSpeciality(){   //pedido sin prescripcion
+ 
+            $scope.dataLoading=true;      //se muestra un data loading mientras se hace el pedido
+            SpecialityService.Create()
+            .then(function(response) {
+                    
+                $scope.dataLoading= false;
+                FlashService.Success('Especialidad agregada',true);  //si funco se muestra un approve
+                
+                },function(response){ 
+                    FlashService.Error(response.status);
+                    $scope.dataLoading= false;
+            });
+        }        
+    
+    }
+} ) ();  // La funcion se auto llama
+///importante no se pueden hacer dos pedidos (con y sin prescripcion al mismo tiempo)
+//
+//
+//
