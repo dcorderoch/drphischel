6f05fd7e07389da0e639cb358f0e5345af083dea
majesco <nicolas.j2007@gmail.com>
2016-05-05 20:57:04 -0600

    Sync controller ans sync view

---
+++ Webpage/login/login.controller.js
@@ -0,0 +1,54 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')   //Pertenece al modulo app de app.js
+        .controller('LoginController', LoginController);//Nombre del controlador, se debe poner 2 veces.
+
+
+//Injeccion de dependencias: Location: para URL, AuthenServ.. $scope se utiliza para variables locales que se pueden 
+//usar en la vista tambien. http es lo de rest. 
+    LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService','$scope','$http', '$rootScope'];
+    function LoginController($location, AuthenticationService, FlashService, $scope, $http, $rootScope) {
+
+        var vm = this; //se pueden utilizar objetos y variables de las inyecciones en la vista
+
+        login = login;
+        userInfo=[]; //variable para obtener los valores retornados del login, almacenado temporal
+
+        (function initController() {
+// Se inicializa el servicio de limpiar credenciales en AuthenticationService
+            AuthenticationService.ClearCredentials();
+        }) ();//Estos parentesis del final indican que esto se inicia automaticamente
+
+        function login() {
+            $scope.dataLoading = true;  //Aqui se hace el rest sin llamar a authentication service porque fue mas facil para mi
+            AuthenticationService.Login( ID, Pass)
+                .then(function(response)  {
+                    
+                    
+                
+                    AuthenticationService.SetCredentials(vm.ID, vm.Pass);
+                    vm.userInfo = response.data; 
+                    $rootScope.company = vm.userInfo[6];//la compania para una variable global
+                    $rootScope.sucursal = vm.userInfo[8];//la sucursal para una variable global
+                    console.log($rootScope.sucursal);
+                    if ( vm.userInfo[7] ==="2" ){ //se determina si es un gerente o dependiente
+                        $location.path('/stats'); //vista de stats
+                    }
+                    if (vm.userInfo[7] ==="1"){
+                        $location.path('/'); //vista de pedidos
+                    }  
+                    else{
+                    FlashService.Error(response.message);//errores
+                    vm.dataLoading = false;  
+                    } 
+                }, 
+                function(response) {             
+                  FlashService.Error(response.message);//errores
+                    $scope.dataLoading = false;   
+                });
+            }
+
+        }
+}) ();
---
+++ Webpage/sync/sync.controller.js
@@ -0,0 +1,28 @@
+(function () {
+    'use strict';
+ 
+    angular
+        .module('app')
+        .controller('SyncController', SyncController);
+ 
+    SyncController.$inject = ['$location', 'MedicineService', 'FlashService', '$rootScope','$scope'];
+    function SyncController(location, MedicineService, FlashService, $rootScope,$scope) {
+        var vm = this;
+ 
+
+        $scope.sync= sync;
+
+        
+        function sync() {
+            MedicineService.GetAll()
+                .then(function (patients) {
+                 FlashService.Success("Se sincronizaron los medicamentos");                          
+            },function(){
+                 FlashService.Error("Error al sincronizar los medicamentos");       
+            });
+        }
+
+
+    }
+ 
+})();
