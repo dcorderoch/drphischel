7295f2d2188989fc32e487abd1dd7e40da6b3083
Kevin <kevgiso@hotmail.com>
2016-05-07 10:26:22 -0600

    Add DBPatient file.

---
+++ mdphischel/mdphischel/DAL/DBPatient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+using mdphischel.DAL.Models;
+
+namespace mdphischel.DAL
+{
+    public class DBPatient
+    {
+        
+
+
+    }
+}
