1e340c0509f2a41c77959636a4d7164d6ee1abb8
Kevin <kevgiso@hotmail.com>
2016-05-09 14:42:39 -0600

    Add DBUser file to project.

---
+++ mdphischel/mdphischel/DAL/DBUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace mdphischel.DAL
+{
+    public class DBUser
+    {
+        public int[] AcceptLogin(int userId, int role)
+        {
+            
+        }
+    }
+}
