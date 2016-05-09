78afa7b21712072455644572885b024d609a2a76
Kevin <kevgiso@hotmail.com>
2016-05-08 21:56:22 -0600

    Complete delete appointment and get appointment by doctorId methods.

--- mdphischel/mdphischel/DAL/DBAppointment.cs
+++ mdphischel/mdphischel/DAL/DBAppointment.cs
@@ -72,7 +72,14 @@ namespace mdphischel.DAL
 
 
         //uspUpdateAppointment @UserId int, @DoctorId nvarchar(15), @OldAppointment DATETIME, @NewAppointment DATETIME
-
+        /// <summary>
+        /// Function in charge of updating an appointment.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="doctorId"></param>
+        /// <param name="oldAppointment"></param>
+        /// <param name="newAppointment"></param>
+        /// <returns>Result codes indicating whether the operation was successful or not.</returns>
         public int[] UpdateAppointment(int userId, string doctorId, string oldAppointment, string newAppointment)
         {
             int[] resultCodes = new int[2];
@@ -133,6 +140,126 @@ namespace mdphischel.DAL
             }
             return resultCodes;
         }
+
+        /// <summary>
+        /// Function in charge of deleting an appointment.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="doctorId"></param>
+        /// <param name="appointmentDate"></param>
+        /// <returns>Result codes indicating whether the operation was successful or not.</returns>
+        public int[] DeleteAppointment(int userId, string doctorId, string appointmentDate)
+        {
+            int[] resultCodes = new int[2];
+            using (SqlConnection connection = new SqlConnection(DBConfigurator.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("uspDeleteAppointment", connection))
+            {
+                SqlParameter errorCodeParameter = cmd.Parameters.Add("@errorNum", SqlDbType.Int);
+                errorCodeParameter.Direction = ParameterDirection.Output;
+
+                SqlParameter resultCodeParameter = cmd.Parameters.Add("@result", SqlDbType.Int);
+                resultCodeParameter.Direction = ParameterDirection.Output;
+
+                SqlParameter userIdParameter = cmd.Parameters.Add("@UserId", SqlDbType.Int);
+                userIdParameter.Direction = ParameterDirection.Input;
+                userIdParameter.Value = userId;
+
+                SqlParameter doctorIdParameter = cmd.Parameters.Add("@DoctorId", SqlDbType.NVarChar);
+                doctorIdParameter.Direction = ParameterDirection.Input;
+                doctorIdParameter.Value = doctorId;
+
+                SqlParameter appointmentDateParameter = cmd.Parameters.Add("@AppointmentDate", SqlDbType.DateTime);
+                appointmentDateParameter.Direction = ParameterDirection.Input;
+                appointmentDateParameter.Value = appointmentDate;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+                var resultCode = resultCodeParameter.Value;
+                var errorNumCode = errorCodeParameter.Value;
+                if (resultCode != DBNull.Value)
+                {
+                    resultCodes[0] = int.Parse(resultCode.ToString());
+                    if (errorNumCode == DBNull.Value)
+                    {
+                        resultCodes[1] = 0;
+                    }
+                    else
+                    {
+                        resultCodes[1] = int.Parse(errorNumCode.ToString());
+                    }
+
+                }
+                else
+                {
+                    resultCodes[0] = 0;
+                    resultCodes[1] = 0;
+                }
+                connection.Close();
+            }
+            return resultCodes;
+        }
+
+        //uspGetAppointmentsByDoctor @DoctorId
+        /// <summary>
+        /// Obtains all appointments by doctor.
+        /// </summary>
+        /// <param name="doctorId"></param>
+        /// <returns>Result codes indicating whether the operation was successful or not.</returns>
+        public int[] GetAppointmentsByDoctor(string doctorId)
+        {
+            int[] resultCodes = new int[2];
+            using (SqlConnection connection = new SqlConnection(DBConfigurator.ConnectionString))
+            using (SqlCommand cmd = new SqlCommand("uspGetAppointmentsByDoctor", connection))
+            {
+                SqlParameter errorCodeParameter = cmd.Parameters.Add("@errorNum", SqlDbType.Int);
+                errorCodeParameter.Direction = ParameterDirection.Output;
+
+                SqlParameter resultCodeParameter = cmd.Parameters.Add("@result", SqlDbType.Int);
+                resultCodeParameter.Direction = ParameterDirection.Output;
+
+                SqlParameter doctorIdParameter = cmd.Parameters.Add("DoctorId", SqlDbType.NVarChar);
+                doctorIdParameter.Direction = ParameterDirection.Input;
+                doctorIdParameter.Value = doctorId;
+
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                connection.Open();
+                cmd.ExecuteNonQuery();
+
+                var resultCode = resultCodeParameter.Value;
+                var errorNumCode = errorCodeParameter.Value;
+                if (resultCode != DBNull.Value)
+                {
+                    resultCodes[0] = int.Parse(resultCode.ToString());
+                    if (errorNumCode == DBNull.Value)
+                    {
+                        resultCodes[1] = 0;
+                    }
+                    else
+                    {
+                        resultCodes[1] = int.Parse(errorNumCode.ToString());
+                    }
+
+                }
+                else
+                {
+                    resultCodes[0] = 0;
+                    resultCodes[1] = 0;
+                }
+
+                //Closing connection
+                connection.Close();
+            }
+            return resultCodes;
+        }
+
+
+
+
+
 
 
     }
