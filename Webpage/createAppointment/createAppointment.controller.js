d54ede489ec2c7f6ad929d795a20a945ffd35460
majesco <nicolas.j2007@gmail.com>
2016-05-08 21:49:04 -0600

    Add code for Create appointment view and controller

--- Webpage/createAppointment/createAppointment.controller.js
+++ Webpage/createAppointment/createAppointment.controller.js
@@ -5,18 +5,27 @@
         .module('app')
         .controller('CreateAppointmentController', CreateAppointmentController);
 
-    CreateAppointmentController.$inject = ['AppointmentService', '$rootScope', 'FlashService', '$scope'];
-    function CreateAppointmentController(AppointmentService, $rootScope, FlashService, $scope) {
+    CreateAppointmentController.$inject = ['AppointmentService', 'DoctorService', '$rootScope', 'FlashService', '$scope'];
+    function CreateAppointmentController(AppointmentService, DoctorService, $rootScope, FlashService, $scope) {
         
         $scope.newAppointment={};
+        $scope.allDoctors=[];
         var vm = this;
         
         $scope.createA= createA;  //variables locales y para vistas
+        
+        initController();
+        
+        function initController(){
+            
+            loadDoctors();
+            console.log($scope.allDoctors);
+        }
+        
 
         function createA(DoctorId){   //pedido sin prescripcion
  
             $scope.dataLoading=true;      //se muestra un data loading mientras se hace el pedido
-            newAppointment.DoctorId = DoctorId;
             newAppointment.UserId = $rootScope.userId;
             AppointmentService.Create($scope.newAppointment)
             .then(function(response) {
@@ -28,8 +37,18 @@
                     FlashService.Error(response.status);
                     $scope.dataLoading= false;
             });
-        }        
+        }
         
+        function loadDoctors(){
+            $scope.allDoctors.push({"x":"b","y":"c"});
+            DoctorService.GetByPatient($rootScope.userId)
+                .then(function(doctors){
+                       $scope.allDoctors = doctors.data;
+                }, function(){
+                    FlashService.Error("Error al cargar doctores del usuario"); 
+                });
+        }
+
 
     
     }
