ea37bfc5c0bf49f2c41c7c0dd19e9995e1f1bd71
Kevin <kevgiso@hotmail.com>
2016-05-09 15:30:13 -0600

    Create User.cs file in DAL Models.

---
+++ mdphischel/mdphischel/DAL/Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mdphischel.DAL.Models
+{
+    public class User
+    {
+        /// <summary>
+        /// Members of class definition.
+        /// </summary>
+        public int UserId { get; set; }
+        public string IdNumber { get; set; }
+        public string Name { get; set; }
+        public string LastName1 { get; set; }
+        public string LastName2 { get; set; }
+        public string BirthDate { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
