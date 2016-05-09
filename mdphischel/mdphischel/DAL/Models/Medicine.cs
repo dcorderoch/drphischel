155c7b392388e0b3397edeff1647b33b05496d08
Kevin <kevgiso@hotmail.com>
2016-05-09 16:49:18 -0600

    Add Appointment.cs and Medicine.cs into DAL Models.

---
+++ mdphischel/mdphischel/DAL/Models/Medicine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mdphischel.DAL.Models
+{
+    public class Medicine
+    {
+        /// <summary>
+        /// Members of class definition.
+        /// </summary>
+        public string BranchOfficeId { get; set; }
+        public string MedicineId { get; set; }
+        public string MedicineName { get; set; }
+        public int Quantity { get; set; }
+        public int Sales { get; set; }
+        public string Price { get; set; }
+
+        public Medicine() { }
+    }
+}
