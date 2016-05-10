6ffb274ee2379e1dc7b846a6db7a3e3b4622b672
Kevin <kevgiso@hotmail.com>
2016-05-10 09:07:40 -0600

    Create accept login method in DBUser.cs.
    Return corresponding User, if exists.

--- mdphischel/mdphischel/DAL/DBUser.cs
+++ mdphischel/mdphischel/DAL/DBUser.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using mdphischel.DAL.Models;
 
 namespace mdphischel.DAL
 {
     public class DBUser
     {
-        public int[] AcceptLogin(int userId, int role)
+        /// <summary>
+        /// Method in charge of validating the user login.
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="role"></param>
+        /// <returns>User, if exists.</returns>
+        public User AcceptLogin(int userId, int role)
         {
             
         }
