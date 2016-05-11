98270ea8c8169bccf0d6cc515321c0f2003f235f
Manuel Zumbado <mazumbado@estudiantec.cr>
2016-05-11 16:54:16 -0600

    Add properties to model

--- mdphischel/mdphischel/DAL/Models/Doctor.cs
+++ mdphischel/mdphischel/DAL/Models/Doctor.cs
@@ -12,6 +12,11 @@ namespace mdphischel.DAL.Models
         /// </summary>
         public string DoctorId { get; set; }
         public int UserId { get; set; }
+        public string Name { get; set; }
+        public string LastName1 { get; set; }
+        public string LastName2 { get; set; }
+        public string PlaceResidence { get; set; }
+        public string BirthDate { get; set; }
         public string OfficeAddress { get; set; }
         public bool IsActive { get; set; }
         public string CreditCardNumber { get; set; }
