6118f55e2ffe6d4114d44c7029b52f068c8913b6
Kevin <kevgiso@hotmail.com>
2016-05-11 05:45:14 -0600

    Add MedicalSpecialtyManager in BLL and complete CreateSpecialty method.

---
+++ mdphischel/mdphischel/BLL/MedicalSpecialtyManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mdphischel.DAL;
+
+namespace mdphischel.BLL
+{
+    /// <summary>
+    /// Class in charge of validating business rules relative to medical specialties.
+    /// </summary>
+    public class MedicalSpecialtyManager
+    {
+        /// <summary>
+        /// Creates a new medical specialty.
+        /// </summary>
+        /// <param name="medicalSpecialty"></param>
+        /// <returns>Integer indicating whether or not the operation was successful.</returns>
+        public int CreateMedicalSpecialty(string medicalSpecialty)
+        {
+            int result;
+            try
+            {
+                DBMedicalSpecialty specialtyInstance = new DBMedicalSpecialty();
+                var operationResult = specialtyInstance.CreateMedicalSpecialty(medicalSpecialty);
+                if (operationResult[0].Equals(Constants.SUCCESS))
+                {
+                    result = Constants.SUCCESS;
+                }
+                else
+                {
+                    result = Constants.ERROR;
+                }
+            }
+            catch (Exception)
+            {
+                result = Constants.ERROR;
+            }
+            return result;
+        }
+    }
+
+
+
+}
