ad3753982e1a38357735313b72b1632cf5b5d7d5
Kevin <kevgiso@hotmail.com>
2016-05-10 20:19:12 -0600

    Finish Doctor DAL Model.

--- mdphischel/mdphischel/DAL/Models/Doctor.cs
+++ mdphischel/mdphischel/DAL/Models/Doctor.cs
@@ -7,5 +7,13 @@ namespace mdphischel.DAL.Models
 {
     public class Doctor
     {
+        /// <summary>
+        /// Members of class definition.
+        /// </summary>
+        public string DoctorId { get; set; }
+        public int UserId { get; set; }
+        public string OfficeAddress { get; set; }
+        public bool IsActive { get; set; }
+        public string CreditCardNumber { get; set; }
     }
 }
