1cc92d397895ba594b611d5388c624746866f74a
Kevin <kevgiso@hotmail.com>
2016-05-11 05:26:35 -0600

    Complete MedicineManager in BLL.

--- mdphischel/mdphischel/BLL/MedicineManager.cs
+++ mdphischel/mdphischel/BLL/MedicineManager.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using mdphischel.DAL;
 using mdphischel.DAL.Models;
 
@@ -105,7 +103,5 @@ namespace mdphischel.BLL
             }
             return retVal;
         }
-
-
     }
 }
