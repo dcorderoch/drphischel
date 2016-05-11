bcf6d86f4f1122a0a70bf455132b533124d67967
Kevin <kevgiso@hotmail.com>
2016-05-11 02:51:57 -0600

    Add BLL Constants file. Success = 1, Error = 0.

---
+++ mdphischel/mdphischel/BLL/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mdphischel.BLL
+{
+    public static class Constants
+    {
+        public const int SUCCESS = 1;
+        public const int ERROR = 0;
+    }
+}
