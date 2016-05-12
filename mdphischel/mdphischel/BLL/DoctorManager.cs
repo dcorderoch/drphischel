29139b96f08990fccf524b485e2d92a9265e3172
Manuel Zumbado <mazumbado@estudiantec.cr>
2016-05-11 19:10:10 -0600

    Add few more methods

--- mdphischel/mdphischel/BLL/DoctorManager.cs
+++ mdphischel/mdphischel/BLL/DoctorManager.cs
@@ -2,10 +2,93 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using mdphischel.DAL;
+using mdphischel.DAL.Models;
 
 namespace mdphischel.BLL
 {
     public class DoctorManager
     {
+        /// <summary>
+        /// This method calls DAL to preregister a new doctor. 
+        /// </summary>
+        /// <param name="docCode"></param>
+        /// <param name="pass"></param>
+        /// <param name="idNumber"></param>
+        /// <param name="name"></param>
+        /// <param name="lastName1"></param>
+        /// <param name="lastName2"></param>
+        /// <param name="residencePlace"></param>
+        /// <param name="birthdate"></param>
+        /// <param name="officeAddres"></param>
+        /// <param name="creditCardNum"></param>
+        /// <returns>1 if succes, 0 if found any errors</returns>
+        public int PreregisterDoctor(String docCode, String pass, String idNumber, String name, String lastName1, String lastName2, String residencePlace, String birthdate, String officeAddres, String creditCardNum)
+        {
+            DBDoctor doctorDAL= new DBDoctor();
+            int[] result=doctorDAL.PreRegistation(docCode, pass, idNumber, name, lastName1, lastName2, residencePlace, birthdate,
+                officeAddres, creditCardNum);
+            return result[0];
+        }
+
+
+        /// <summary>
+        /// This method calls DAL to accept a pre registed doctor
+        /// </summary>
+        /// <param name="docCode"></param>
+        /// <returns>1 if succes, 0 if found errors</returns>
+        public int AcceptDoctor(string docId)
+        {
+            DBDoctor doctorDAL = new DBDoctor();
+            int[] result = doctorDAL.AcceptDoctor(docCode);
+            return result[0];
+        }
+
+        /// <summary>
+        /// This method calls DAL to reject a pre registed doctor
+        /// </summary>
+        /// <param name="doctorId"></param>
+        /// <returns></returns>
+        public int RejectDoctor(string doctorId)
+        {
+            DBDoctor doctorDAL = new DBDoctor();
+            int[] result = doctorDAL.RejectDoctor(doctorId);
+            return result[0];
+        }
+
+        /// <summary>
+        /// This method calls DAL to retrieve information about doctors' charges
+        /// </summary>
+        /// <param name="year">Must be a string in YYYY format</param>
+        /// <param name="month">Must be a string in MM format</param>
+        /// <returns></returns>
+        public List<DoctorsCharge> GetMonthlyCharges(string year, string month)
+        {
+            string date = year + month + "01";
+            DBDoctor doctorDAL = new DBDoctor();
+            MonthlyDocCharges docCharges = doctorDAL.GetMonthlyCharges(date);
+            return docCharges.ChargesList;
+        }
+
+        /// <summary>
+        /// Get pending doctors
+        /// </summary>
+        /// <returns></returns>
+        public List<Doctor> GetPendingDoctors()
+        {
+            DBDoctor doctorDAL = new DBDoctor();
+            return doctorDAL.GetPendingDoctors();
+        }
+
+        /// <summary>
+        /// Get patients by doctor id
+        /// </summary>
+        /// <param name="doctorId"></param>
+        /// <returns></returns>
+        public List<User> GetPatientsByDoctorId(string doctorId)
+        {
+            DBDoctor doctorDAL = new DBDoctor();
+            return doctorDAL.GetPatientsByDoctorId(doctorId);
+        } 
     }
 }
