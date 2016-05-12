9c9185eeb44b96a3adf70b73b172ffea943865ca
Manuel Zumbado <mazumbado@estudiantec.cr>
2016-05-11 19:24:24 -0600

    Add AddMedicineIntoPrescription method

--- mdphischel/mdphischel/BLL/PrescriptionManager.cs
+++ mdphischel/mdphischel/BLL/PrescriptionManager.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using mdphischel.DAL;
 
 namespace mdphischel.BLL
 {
     public class PrescriptionManager
     {
+
+        /// <summary>
+        /// This method calls DAL to add a medicine into prescription
+        /// </summary>
+        /// <param name="medicineId"></param>
+        /// <param name="prescriptionId"></param>
+        /// <returns></returns>
+        public int AddMedicineIntoPrescription(string medicineId, string prescriptionId)
+        {
+            DBPrescription prescriptionDAL= new DBPrescription();
+            int[] result = prescriptionDAL.AddMedicineIntoPrescription(medicineId, prescriptionId);
+            return result[0];
+        }
     }
 }
