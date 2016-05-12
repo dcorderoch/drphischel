22f5d9da419087ffca11ed42295205ae05f6b900
Manuel Zumbado <mazumbado@estudiantec.cr>
2016-05-11 19:18:17 -0600

    Add GetMedicalRecords method

--- mdphischel/mdphischel/BLL/MedicalRecordManager.cs
+++ mdphischel/mdphischel/BLL/MedicalRecordManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using mdphischel.DAL;
+using mdphischel.DAL.Models;
 
 namespace mdphischel.BLL
 {
@@ -26,6 +27,32 @@ namespace mdphischel.BLL
             return result[0];
         }
 
-        
+        /// <summary>
+        /// This method calls DAL to update an existing medicalrecordentry
+        /// </summary>
+        /// <param name="medicalRecordId"></param>
+        /// <param name="appointmentId"></param>
+        /// <param name="description"></param>
+        /// <param name="diagnosis"></param>
+        /// <param name="prescriptionId"></param>
+        /// <returns>1 if success, 0 if error</returns>
+        public int UpdateMedicalRecordEntry(int medicalRecordId, int appointmentId, string description, string diagnosis, string prescriptionId)
+        {
+            DBMedicalRecord medicalRecordDAL=new DBMedicalRecord();
+            int[] result = medicalRecordDAL.UpdateMedicalRecordEntry(medicalRecordId, appointmentId, description,
+                diagnosis, prescriptionId);
+            return result[0];
+        }
+
+        /// <summary>
+        /// This method gets all the records for the given patient id
+        /// </summary>
+        /// <param name="patientId"></param>
+        /// <returns></returns>
+        public List<MedicalRecord> GetMedicalRecords(int patientId)
+        {
+            DBMedicalRecord medicalRecordDAL=new DBMedicalRecord();
+            return medicalRecordDAL.GetMedicalRecords(patientId);
+        } 
     }
 }
