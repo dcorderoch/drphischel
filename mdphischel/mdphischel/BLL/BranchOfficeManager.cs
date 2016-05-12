8074c238f30d624efcfd540d65cc78e0defee531
Manuel Zumbado <mazumbado@estudiantec.cr>
2016-05-11 18:47:25 -0600

    Add GetAllBranchOffices Method

--- mdphischel/mdphischel/BLL/BranchOfficeManager.cs
+++ mdphischel/mdphischel/BLL/BranchOfficeManager.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using mdphischel.DAL;
+using mdphischel.DAL.Models;
 
 namespace mdphischel.BLL
 {
     public class BranchOfficeManager
     {
+
+        public List<BranchOffice> GetAllBranchOffices(string doctorId)
+        {
+            DBBranchOffice branchOfficeDAL = new DBBranchOffice();
+            return branchOfficeDAL.GetPrescriptionByDoctor(doctorId);
+        }
     }
 }
